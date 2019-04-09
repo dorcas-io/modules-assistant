3f8925963169921cb1de01e41ce9922702bc874f
Ifeoluwa <bolaji.olawoye@hostvillenigeria.org>
2019-04-09 18:55:17 +0100

    Modules Structure Setup

---
+++ src/Models/ModulesAssistant.php
@@ -0,0 +1,10 @@
+<?php
+// MyVendor\Contactform\src\Models\ContactForm.php
+namespace Dorcas\ModulesAssistant\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class ModulesAssistant extends Model
+{
+    protected $guarded = [];
+    //protected $table = 'modules-assistant';
+}
---
+++ src/ModulesAssistantServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Dorcas\ModulesSales;
+use Illuminate\Support\ServiceProvider;
+
+class ModulesSalesServiceProvider extends ServiceProvider {
+
+	public function boot()
+	{
+		$this->loadRoutesFrom(__DIR__.'/routes/web.php');
+		$this->loadViewsFrom(__DIR__.'/resources/views', 'modules-assistant');
+		$this->publishes([
+			__DIR__.'/config/modules-assistant.php' => config_path('modules-assistant.php'),
+		], 'config');
+		/*$this->publishes([
+			__DIR__.'/assets' => public_path('vendor/modules-assistant')
+		], 'public');*/
+	}
+
+	public function register()
+	{
+		//add menu config
+		$this->mergeConfigFrom(
+	        __DIR__.'/config/navigation-menu.php', 'navigation-menu.modules-assistant.sub-menu'
+	     );
+	}
+
+}
+
+
+?>
