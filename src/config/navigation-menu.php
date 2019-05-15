fbf4de8a34fdd3981a91a4c17a897d0d57b972cc
Ifeoluwa <bolaji.olawoye@hostvillenigeria.org>
2019-05-15 23:02:01 +0100

    Assistant Beta

--- src/config/navigation-menu.php
+++ src/config/navigation-menu.php
@@ -1,4 +1,13 @@
 <?php
 return [
-
+    'assistant-chat' => [
+        'title' => 'Chat',
+        'route' => 'assistant-chat',
+        'icon' => 'fe fe-message-circle',
+    ],
+    'assistant-call' => [
+    	'title' => 'Call',
+    	'route' => 'assistant-call',
+        'icon' => 'fe fe-phone',
+    ]
 ];
